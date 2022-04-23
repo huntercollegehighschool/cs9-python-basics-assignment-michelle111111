a00e4d88d3d7464f4e9eb06f036917a451d01fb6
Michelle <103528504+michelle111111@users.noreply.github.com>
2022-04-23 17:49:09 +0000

    did parts 3-5

--- part5.py
+++ part5.py
@@ -11,5 +11,10 @@ What is your last name? Ketchum
 Hello, Ash Ketchum
 
 '''
+firstN = input("What is your first name? ")
+lastN = input("What is your last name? ")
 
+name = firstN + " " + lastN
+
+print("Hello", name)
 #start writing your code below
